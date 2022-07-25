a1a0ebe94c8908c740e125dd47e731df3cca389b
Davin Alfarizky Putra Basudewa <dbasudewa@gmail.com>
2022-07-25 23:35:42 +0700

    Test video

---
+++ components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Player</p>
+  <div ref="mediaPlayer" style="width: 1024px;height: 600px"></div>
+</template>
+<script setup>
+import {nextTick, onMounted, reactive, ref} from 'vue'
+import Artplayer from "artplayer";
+
+const mediaPlayer = ref(null)
+const playerOption = reactive({
+  url: 'https://www.googleapis.com/drive/v3/files/1J1w33vYakUCHsA1VdtaxjfAptjtta7j-?alt=media&key=AIzaSyAmulazgBW2wXbJtPH6RQiTidbXLXI9S9s',
+  title: 'Your Name',
+})
+
+onMounted(() => {
+  nextTick(() => {
+    new Artplayer({
+      ...playerOption,
+      container: mediaPlayer.value
+    })
+  })
+})
+
+defineExpose({
+  mediaPlayer,
+  playerOption
+})
+</script>
+
+<style scoped>
+
+</style>
