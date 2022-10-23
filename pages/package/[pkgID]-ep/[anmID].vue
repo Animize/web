3b735f9df11c2d48a526aa13c473a45bc3531a86
Davin Alfarizky Putra Basudewa <dbasudewa@gmail.com>
2022-10-23 13:13:06 +0700

    [CHORES] Fixing layout anime player

--- pages/package/[pkgID]-ep/[anmID].vue
+++ pages/package/[pkgID]-ep/[anmID].vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="flex justify-center">
-    <Head v-if="!pending">
+  <div class="p-4">
+    <Head v-if="!episodePending">
       <Title>
         {{packages.name ? packages.name : 'Animize'}} | Episode {{episode.episode ? episode.episode : '0'}}
       </Title>
       <Meta :content="episode ?`Episode ${episode.episode}, ${episode.summary ? episode.summary : 'Nothing to be told'}`  : 'Animize'" name="description"/>
     </Head>
     <div v-if="episode && !episodePending" class="flex-inline flex-col">
-      <div class="item flex flex-row flex-wrap w-screen p-4">
-        <div class="item flex-col w-full md:w-2/3 h-auto p-2">
+      <div class="item w-full items-center flex flex-col md:flex-row text-justify m-2 gap-2 p-2">
+        <div class="item w-full md:w-2/3">
           <video-player ref="animizePlayer"
                         :controls="true"
                         :sources="videoPlayerSource"
@@ -16,7 +16,7 @@
                         @mounted="videoPlayerOnMounted"
           />
         </div>
-        <div class="item flex-col w-full md:w-1/3 h-auto">
+        <div class="item w-full md:w-1/3 md:h-96">
           <Disclosure :v-slot="hideEpisodeSelector" as="div"
                       class="dark:bg-gray-800 rounded shadow-lg flex-col h-auto w-auto">
             <DisclosureButton
@@ -51,35 +51,28 @@
           </Disclosure>
         </div>
       </div>
-      <div
-          class="item flex flex-row rounded shadow-lg dark:bg-gray-800 dark:text-white text-gray-800 text-justify ml-8 mr-8 mb-4 p-4">
+      <div class="item w-full items-center flex flex-col md:flex-row relative rounded shadow-lg dark:bg-gray-800
+          dark:text-white text-gray-800 text-justify m-2 gap-2 p-2">
         <nuxt-img :alt="`animize-${pkgID}-cover`"
                   :src="packages.cover ? packages.cover : '/icon/img_notfound.png'"
-                  class="object-cover w-48 h-72 rounded md:h-auto md:w-48 shadow m-8 hover:scale-110"/>
-        <div
-            class="item flex flex-col text-justify">
-          <div class="item flex flex-col text-3xl font-bold flex-wrap w-full pl-4 pt-2">
+                  class="object-cover w-48 aspect-[7/10] rounded shadow"/>
+        <div class="flex flex-col justify-between gap-2 leading-normal h-auto">
+          <div class="item text-3xl font-bold flex-wrap w-full">
             {{ packages.name }}
           </div>
-          <div class="item flex flex-col text-2xs-sm flex-wrap w-full pl-4 pt-2">
+          <div class="item text-2xs-sm flex-wrap w-full">
             {{ packages.synopsis }}
           </div>
         </div>
       </div>
-
-
-      <div
-          class="item flex flex-col dark:text-white text-gray-800 text-justify
-          ml-8 mb-4 mr-8 p-4">
-        <div class="item flex flex-col text-2xl flex-wrap w-full pl-4 pt-2">
+      <div class="item w-full items-center flex-row md:flex-row rounded shadow-lg dark:bg-gray-800
+          dark:text-white text-gray-800 text-justify m-2 gap-2 p-2">
+        <div class="item flex flex-col text-2xl font-bold flex-wrap w-full">
           Sources
         </div>
-        <div class="item flex flex-wrap gap-1.5">
+        <div class="item w-auto h-auto flex flex-wrap gap-2 p-2">
           <div v-for="src in videoPlayerSource"
-               class="px-4 py-2 rounded-full font-semibold text-sm cursor-pointer active:bg-gray-300
-              active:text-gray-800 dark:active:bg-gray-800 dark:active:text-white hover:bg-gray-900
-              hover:text-white dark:hover:bg-gray-300 dark:hover:text-gray-800 transition duration-300 ease
-              text-gray-500 bg-gray-200 dark:bg-gray-800 dark:text-white mr-1">
+               class="chip transition-colors ease-in duration-200">
             {{ src.label }}
           </div>
 
