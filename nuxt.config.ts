dec48361453f7e59574ac23e9c45f40f0fed244a
Davin Alfarizky Putra Basudewa <dbasudewa@gmail.com>
2022-12-30 23:18:46 +0700

    [chores] add meta for SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,7 @@ export default defineNuxtConfig({
     },
     ssr: true,
     runtimeConfig: {
-        public:{
+        public: {
             API_BASE_URL: process.env.API_BASE_URL
         }
     },
@@ -20,4 +20,29 @@ export default defineNuxtConfig({
         '@nuxtjs/tailwindcss',
         '@nuxt/image-edge'
     ],
+    app: {
+        head: {
+            meta: [
+                {
+                    charset: 'utf-8'
+                },
+                {
+                    name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=3.0'
+                },
+                {
+                    hid: 'og:type', name: 'og:type', content: 'website'
+                },
+                {
+                    hid: 'og:site_name', name: 'og:site_name', content: 'Animize'
+                },
+                {
+                    hid: 'og:title', name: 'og:title', content: 'Animize - Streaming community'
+                },
+                {
+                    hid: 'og:description', name: 'og:description',
+                    content: 'Animize is a website to share your fans subbed and stream anime from the web.'
+                }
+            ]
+        }
+    }
 })
