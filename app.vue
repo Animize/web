0f148f4c44ae459db14ba12523116a534639ec29
Davin Alfarizky Putra Basudewa <dbasudewa@gmail.com>
2023-01-04 00:02:59 +0700

    [chores] change components import to using auto import nuxt3

--- app.vue
+++ app.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="flex flex-col animize-background">
     <NuxtLoadingBar :duration="1000" />
-    <Navbar class="item w-auto h-auto flex-auto"></Navbar>
+    <LazyCommonNavbar class="item w-auto h-auto flex-auto"></LazyCommonNavbar>
     <NuxtPage class="item w-auto min-h-screen h-auto flex-grow"></NuxtPage>
-    <AnimizeFooter class="item w-auto h-auto flex-auto"></AnimizeFooter>
+    <LazyCommonFooter class="item w-auto h-auto flex-auto"></LazyCommonFooter>
   </div>
 
 </template>
@@ -11,10 +11,6 @@
 <script setup>
 import {loadTheme} from "@/composables/dark-setting";
 loadTheme()
-
-import Navbar from "@/components/common/Navbar";
-import AnimizeFooter from "@/components/common/Footer";
-
 useHead({
   title: 'Animize'
 })
