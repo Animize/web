5eb721345f1047b6386f0ca755378d417422d113
Davin Alfarizky Putra Basudewa <dbasudewa@gmail.com>
2023-01-12 11:14:48 +0700

    [chores] Change loading indicator

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col animize-background">
-    <NuxtLoadingBar :duration="1000"/>
+    <NuxtLoadingIndicator/>
     <LazyCommonNavbar class="item w-full  fixed top-0 sticky z-20 bg-opacity-80 backdrop-blur-xl"></LazyCommonNavbar>
     <NuxtPage class="item w-auto min-h-screen h-auto flex-grow relative"></NuxtPage>
     <LazyCommonFooter class="item w-auto h-auto flex-auto"></LazyCommonFooter>
