302177467569ecfded4889f88a1a36410660afd2
Davin Alfarizky Putra Basudewa <dbasudewa@gmail.com>
2023-01-21 17:24:41 +0700

    [chores] Change backdrop opacity

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col animize-background">
     <NuxtLoadingIndicator/>
-    <LazyCommonNavbar class="item w-full  fixed top-0 sticky z-20 bg-opacity-80 backdrop-blur-xl"></LazyCommonNavbar>
+    <LazyCommonNavbar class="item w-full  fixed top-0 sticky z-20 bg-opacity-70 backdrop-blur-xl"></LazyCommonNavbar>
     <NuxtPage class="item w-auto min-h-screen h-auto flex-grow relative"></NuxtPage>
     <LazyCommonFooter class="item w-auto h-auto flex-auto"></LazyCommonFooter>
   </div>
--- components/anime/AnimeFilter.vue
+++ components/anime/AnimeFilter.vue
@@ -1,13 +1,13 @@
 <template>
   <TransitionRoot :show="dialogOpen" as="template">
-    <Dialog as="div" class="relative z-10 overflow-y-auto" @close="dialogOpen = false">
+    <Dialog as="div" class="relative z-10" @close="dialogOpen = false">
       <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100"
                        leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
-        <div class="fixed inset-0 backdrop-blur-sm bg-opacity-75 transition-opacity"/>
+        <div class="fixed inset-0 backdrop-blur-sm bg-opacity-60 transition-opacity"/>
       </TransitionChild>
 
-      <div class="fixed z-10 inset-0 overflow-y-auto">
-        <div class="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
+      <div class="fixed z-10 inset-0">
+        <div class="flex items-end sm:items-center justify-center w-screen h-screen p-4 text-center sm:p-0">
           <TransitionChild as="template" enter="ease-out duration-300"
                            enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                            enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200"
