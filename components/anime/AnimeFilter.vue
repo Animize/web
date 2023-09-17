60ad083571f4583043a669415002e47dc4ca9513
Davin Alfarizky Putra Basudewa <dbasudewa@gmail.com>
2023-09-17 22:57:18 +0700

    [CHORES] Create new css class for text input

--- components/anime/AnimeFilter.vue
+++ components/anime/AnimeFilter.vue
@@ -49,7 +49,7 @@
                 </div>
                 <input id="simple-search"
                        v-model="nameSearch"
-                       class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 m-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                       class="text-input"
                        placeholder="Search"
                        type="text">
                 <div class="mt-4 text-left font-semibold text-xl">
--- components/common/Navbar.vue
+++ components/common/Navbar.vue
@@ -39,7 +39,7 @@
                   </svg>
                 </div>
                 <input id="simple-search"
-                       class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                       class="text-input pl-10"
                        placeholder="Search"
                        v-model="search"
                        @keyup.enter="searchAnime(search)"
