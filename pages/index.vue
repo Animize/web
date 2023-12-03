f902bd3e952a07bb2b6025d925d7b1028774cc42
Davin Alfarizky Putra Basudewa <dbasudewa@gmail.com>
2023-12-03 22:59:04 +0700

    [FIX] Fix index

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="flex flex-col">
-    <div v-if="!pending" class="item w-full items-center flex flex-col md:flex-row text-left">
+    <div v-if="!bannerPending" class="item w-full items-center flex flex-col md:flex-row text-left">
       <Carousel
-          :settings="carouselSetting" :breakpoints="carouselBreakpoints" :autoplay="0" :wrapAround="true"
-          :pauseAutoplayOnHover="true" :transition="500" class="w-full h-auto">
-        <Slide v-for="pkg in bannerPackages.data.content" :key="pkg.id">
+          :autoplay="0" :breakpoints="carouselBreakpoints" :pauseAutoplayOnHover="true" :settings="carouselSetting"
+          :transition="500" :wrapAround="true" class="w-full h-auto">
+        <Slide v-for="pkg in bannerPackages?.data?.content" :key="pkg.id">
           <NuxtLink
-              class="flex flex-col md:flex-row relative w-full aspect-[10/5] m-2 z-0 group transition duration-300 ease"
               :to="`/package/${pkg.id}`"
+              class="flex flex-col md:flex-row relative w-full aspect-[10/5] m-2 z-0 group transition duration-300 ease"
           >
             <LazyNuxtImg :alt="`animize-${pkg.id}-cover-blur`" :src="pkg.cover ? pkg.cover : '/icon/img_notfound.png'"
-                     class="object-cover opacity-30 w-full aspect-[10/5] absolute rounded-lg z-0 transition-all duration-300	group-hover:opacity-50 group-hover:blur"/>
+                         class="object-cover opacity-30 w-full aspect-[10/5] absolute rounded-lg z-0 transition-all duration-300	group-hover:opacity-50 group-hover:blur"/>
             <div class="h-fit w-full pr-2 pl-2 bottom-0 visible group-hover:invisible">
               <span class="item animize-text-subtitle line-clamp-3">
                 {{ pkg.name }}</span>
@@ -31,10 +31,10 @@
         </Slide>
       </Carousel>
     </div>
-    <div class="item flex flex-col md:flex-row">
+    <div v-if="credential" class="item flex flex-col md:flex-row">
       <div class="basis-1/2 h-96 card p-2 m-2 flex flex-col">
         <LazyCommonShimmerCard v-if="historyPending"/>
-        <div v-if="!historyPending &&credential && history?.data?.totalElements > 0"
+        <div v-if="!historyPending && history?.data?.totalElements > 0"
              class="item relative flex items-center justify-between m-2">
           <div class="left-0 animize-text-subtitle">
             Resume what you left
@@ -43,10 +43,10 @@
             See more
           </div>
         </div>
-        <div v-if="!historyPending &&credential && history?.data?.totalElements > 0"
+        <div v-if="!historyPending && history?.data?.totalElements > 0"
              class="item w-full h-full overflow-y-auto">
           <div v-for="hst in history?.data?.content" :key="hst?.id" class="flex flex-row p-4">
-            <LazyNuxtImg class="basis-1/4 aspect-[7/10] w-4" :src="hst?.packages?.cover ?? '/icon/img_notfound.png'"
+            <LazyNuxtImg :src="hst?.packages?.cover ?? '/icon/img_notfound.png'" class="basis-1/4 aspect-[7/10] w-4"
                          placeholder/>
             <div class="basis-3/4">
               <div class="animize-text-subtitle p-2 line-clamp-2">
@@ -67,7 +67,7 @@
       </div>
       <div class="basis-1/2 h-96 card p-2 m-2 flex flex-col">
         <LazyCommonShimmerCard v-if="favoritesPending"/>
-        <div v-if="!favoritesPending && credential && favorites?.data?.totalElements > 0"
+        <div v-if="!favoritesPending && favorites?.data?.totalElements > 0"
              class="item relative flex items-center justify-between m-2">
           <div class="left-0 animize-text-subtitle">
             Your favorited anime
@@ -76,11 +76,11 @@
             See more
           </div>
         </div>
-        <div v-if="!favoritesPending && credential && favorites?.data?.totalElements > 0"
+        <div v-if="!favoritesPending && favorites?.data?.totalElements > 0"
              class="item w-full h-full overflow-y-auto">
           <div v-for="fvr in favorites?.data?.content" :key="fvr?.id" class="flex flex-row p-4">
-            <LazyNuxtImg class="basis-1/4 aspect-[7/10] w-4" :src="fvr?.packages?.cover ?? '/icon/img_notfound.png'"
-            placeholder/>
+            <LazyNuxtImg :src="fvr?.packages?.cover ?? '/icon/img_notfound.png'" class="basis-1/4 aspect-[7/10] w-4"
+                         placeholder/>
             <div class="basis-3/4">
               <div class="animize-text-subtitle p-2 line-clamp-2">
                 {{ fvr?.packages?.name }}
@@ -104,7 +104,7 @@
   </div>
 
 </template>
-<script setup lang="ts">
+<script lang="ts" setup>
 import {Carousel, Slide} from 'vue3-carousel'
 import 'vue3-carousel/dist/carousel.css'
 import {convertSecondToVideoDurationFormat} from "~/composables/util/VideoUtils";
@@ -122,30 +122,43 @@ const carouselBreakpoints = {
   }
 }
 
-const {data: bannerPackages, pending, refresh} = await useLazyAsyncData(
+const {data: bannerPackages, pending: bannerPending, refresh: bannerRefresh} = await useLazyAsyncData(
     'bannerPackages',
-    () => $fetch<any>(`${config.public.API_BASE_URL}/packages/page?page=0&size=10&sort=name,desc`)
+    () => useAPI<ResponsePageDTO>(`${config.public.API_BASE_URL}/packages/page?page=0&size=10&sort=name,desc`)
 )
 
 
 const {data: history, pending: historyPending, refresh: historyRefresh} = await useLazyAsyncData(
     'homeHistory',
-    () => useAPI<any>(`${config.public.API_BASE_URL}/myself/watch?page=0&size=5&sort=lastModifiedDate,desc`),
+    () => useAPI<ResponsePageDTO>(`${config.public.API_BASE_URL}/myself/watch?page=0&size=5&sort=lastModifiedDate,desc`),
     {
       immediate: false,
-
+      watch: [
+        credential
+      ]
     }
 )
 
 const {data: favorites, pending: favoritesPending, refresh: favoritesRefresh} = await useLazyAsyncData(
     'homeFavorites',
-    () => useAPI<any>(`${config.public.API_BASE_URL}/myself/favorites?page=0&size=5&sort=createdDate,desc`),
+    () => useAPI<ResponsePageDTO>(`${config.public.API_BASE_URL}/myself/favorites?page=0&size=5&sort=createdDate,desc`),
     {
       immediate: false,
-
+      watch: [
+        credential
+      ]
     }
 )
 
+onMounted(async () => {
+  await bannerRefresh()
+  if (credential) {
+    await Promise.all([
+      historyRefresh(),
+      favoritesRefresh()
+    ])
+  }
 
-defineExpose({})
+})
+
 </script>
